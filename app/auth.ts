6456b610a941d862e3bae12adb384c8708096124
Mauro0407 <130101420+Mauro0407@users.noreply.github.com>
2025-11-28 01:29:32 -0500

    Mejoras al proyecto

---
+++ app/auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth" 
+import Auth0 from "next-auth/providers/auth0" 
+  
+export const { handlers, auth, signIn, signOut } = 
+NextAuth({ 
+ 
+  providers: [ 
+    Auth0({ 
+        name: "Auth0", 
+        clientId: process.env.AUTH_AUTH0_ID, 
+        clientSecret: process.env.AUTH_AUTH0_SECRET, 
+        issuer: `https://${process.env.AUTH_AUTH0_DOMAIN}` 
+    }) 
+  ], 
+ 
+})
---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+import HeaderClient from "./Header.client"; 
+import { auth } from "../auth"; 
+const Header = async () => { 
+const session = await auth(); 
+return <HeaderClient user={session?.user ?? null} />; 
+}; 
+export default Header; 
